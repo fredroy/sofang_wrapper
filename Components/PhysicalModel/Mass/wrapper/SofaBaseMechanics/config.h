4947afef5cb6a2a2d4d14f4c2f96e87cfaf28736
Frederick Roy <fred.roy.me@gmail.com>
2021-04-15 19:23:22 +0200

    add mass

---
+++ Components/PhysicalModel/Mass/wrapper/SofaBaseMechanics/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <sofa/components/physicalmodels/mass/config.h>
+
+#ifdef SOFA_BUILD_SOFABASEMECHANICS
+#  define SOFA_SOFABASEMECHANICS_API SOFA_EXPORT_DYNAMIC_LIBRARY
+#else
+#  define SOFA_SOFABASEMECHANICS_API SOFA_IMPORT_DYNAMIC_LIBRARY
+#endif
---
+++ TODO.md
@@ -0,0 +1,5 @@
+# Sofa.Components.Topology
+- Remove RadiusContainer (from SofaGeneralFEM) in EdgeToQuad
+
+# Sofa.Components.Constraint
+- Remove use of UniformMass from UncoupledConstraintCorrection
