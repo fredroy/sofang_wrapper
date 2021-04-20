a216b0ec1121d1f6eae15d800e4bc563931c3bf9
Frederick Roy <fred.roy.me@gmail.com>
2021-04-20 13:36:43 +0200

    add collision algo (detection)

---
+++ Components/Collision/Detection/Algorithm/wrapper/SofaGeneralMeshCollision/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <sofa/components/collision/detection/algorithm/config.h>
+
+#ifdef SOFA_BUILD_SOFAGENERALMESHCOLLISION
+#  define SOFA_SOFAGENERALMESHCOLLISION_API SOFA_EXPORT_DYNAMIC_LIBRARY
+#else
+#  define SOFA_SOFAGENERALMESHCOLLISION_API SOFA_IMPORT_DYNAMIC_LIBRARY
+#endif
