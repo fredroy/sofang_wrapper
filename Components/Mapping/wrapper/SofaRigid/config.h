9de60b857a1fc96bf9656d4644375b3de47af88b
Frederick Roy <fred.roy.me@gmail.com>
2021-04-20 13:24:46 +0200

    add mapping

---
+++ Components/Mapping/wrapper/SofaRigid/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <sofa/components/mapping/config.h>
+
+#ifdef SOFA_BUILD_SOFARIGID
+#  define SOFA_SOFARIGID_API SOFA_EXPORT_DYNAMIC_LIBRARY
+#else
+#  define SOFA_SOFARIGID_API SOFA_IMPORT_DYNAMIC_LIBRARY
+#endif
