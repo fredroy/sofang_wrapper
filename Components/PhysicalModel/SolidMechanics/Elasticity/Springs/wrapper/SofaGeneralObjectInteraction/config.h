f6f393a626fc661d75fe2a02718c41bc39eceb4b
Frederick Roy <fred.roy.me@gmail.com>
2021-04-20 19:15:56 +0200

    update springs

---
+++ Components/PhysicalModel/SolidMechanics/Elasticity/Springs/wrapper/SofaGeneralObjectInteraction/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <sofa/components/physicalmodel/solidmechanics/elasticity/springs/config.h>
+
+#ifdef SOFA_BUILD_SOFAGENERALOBJECTINTERACTION
+#  define SOFA_SOFAGENERALOBJECTINTERACTION_API SOFA_EXPORT_DYNAMIC_LIBRARY
+#else
+#  define SOFA_SOFAGENERALOBJECTINTERACTION_API SOFA_IMPORT_DYNAMIC_LIBRARY
+#endif
