4519b42c807edab5dc1919efc8fb039bcd9ea936
Frederick Roy <frederick.roy@inria.fr>
2021-07-02 11:37:54 +0200

    follow master

---
+++ Components/Collision/Detection/Intersection/wrapper/SofaMiscCollision/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <sofa/components/collision/models/config.h>
+
+#ifdef SOFA_BUILD_SOFAMISCCOLLISION
+#  define SOFA_MISC_COLLISION_API SOFA_EXPORT_DYNAMIC_LIBRARY
+#else
+#  define SOFA_MISC_COLLISION_API SOFA_IMPORT_DYNAMIC_LIBRARY
+#endif
