28cc84d1258d4542403636c16fc52044b4446da6
Frederick Roy <frederick.roy@inria.fr>
2021-09-21 14:45:00 +0200

    move gl check, add loading ability to visual

--- Components/Visual/src/sofa/components/visual/initMinimal.cpp
+++ Components/Visual/src/sofa/components/visual/initMinimal.cpp
@@ -1,6 +1,25 @@
 #include <sofa/components/visual/config.h>
 
-namespace sofa::component::visual
+namespace sofa::components::visual
 {
 
-} // namespace sofa::component::visual
+extern "C" {
+    SOFACOMPONENTSVISUAL_API void initExternalModule();
+    SOFACOMPONENTSVISUAL_API const char* getModuleName();
+}
+
+void initExternalModule()
+{
+	static bool first = true;
+	if (first)
+	{
+		first = false;
+	}
+}
+
+const char* getModuleName()
+{
+	return MODULE_NAME;
+}
+
+} // namespace sofa::components::visual
