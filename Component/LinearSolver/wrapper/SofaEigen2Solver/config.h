02fe3b14ab80e6111235d3bdabe672d6d70b3809
Frederick Roy <frederick.roy@inria.fr>
2021-10-07 11:32:55 +0200

    follow sofa master (remove sofaeigen2solver)

--- Component/LinearSolver/wrapper/SofaEigen2Solver/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <sofa/component/linearsolver/config.h>
-
-#ifdef SOFA_BUILD_SOFAEIGEN2SOLVER
-#  define SOFA_SOFAEIGEN2SOLVER_API SOFA_EXPORT_DYNAMIC_LIBRARY
-#else
-#  define SOFA_SOFAEIGEN2SOLVER_API SOFA_IMPORT_DYNAMIC_LIBRARY
-#endif
