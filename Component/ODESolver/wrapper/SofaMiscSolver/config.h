6ba3a6e4a11fe951c0e0052301ef84ef86a3899a
Frederick Roy <frederick.roy@inria.fr>
2021-10-27 16:02:39 +0200

    splt odesolver

--- Component/ODESolver/wrapper/SofaMiscSolver/config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <sofa/component/odesolver/config.h>
-
-#ifdef SOFA_BUILD_SOFAMISCSOLVER
-#  define SOFA_SOFAMISCSOLVER_API SOFA_EXPORT_DYNAMIC_LIBRARY
-#else
-#  define SOFA_SOFAMISCSOLVER_API SOFA_IMPORT_DYNAMIC_LIBRARY
-#endif
